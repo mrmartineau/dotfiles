0b6f9a17f40fb6c832450c7af70efb373cad3d5c
Zander Martineau <zander@martineau.tv>
2016-02-13 10:57:18 +0000

    add ntl & eslint. remove jscs and jshint. reorganise

--- npm.sh
+++ npm.sh
@@ -2,40 +2,40 @@ NPM=(
 	grunt-cli
 	gulp
 	webpack
-	
-	yo
-	bower
-	generator-kickoff
-	generator-generator
-	
-	nodemon
-	npm-check-updates
-	
-	jshint
-	jscs
-	stylestats
-	csslint
-	
-	tmi
-	psi
-	a11y
-	
+
 	browserify
 	watchify
 	disc
 	inherits
-	
+
+	yo
+	bower
+	generator-kickoff
+	generator-generator
+
+	nodemon
+	npm-check-updates
+	ntl
+
+	eslint
+	stylestats
+	csslint
+
+	tmi
+	psi
+	a11y
+
 	karma-cli
 	istanbul
 	jasmine
 	phantomas
-	
+
 	gh
 	gg
 	dploy
-	
+
 	vtop
-	
+
 	finch
 )
 
